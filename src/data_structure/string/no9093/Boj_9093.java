499e9ff06b07d477f772b2b2738ecbbf0391e189
HyunSik Ryu <hsryu123@naver.com>
2023-03-29 21:29:39 +0900

    BOJ

---
+++ src/data_structure/string/no9093/Boj_9093.java
@@ -0,0 +1,30 @@
+package data_structure.string.no9093;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Boj_9093 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+        StringTokenizer st;
+
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=0;i<n;i++) {
+            st = new StringTokenizer(br.readLine());
+            
+            while (st.hasMoreTokens()) {
+                String str = st.nextToken();
+                StringBuffer stb = new StringBuffer(str);
+                sb.append(stb.reverse().toString()+" ");
+            }
+
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+}
