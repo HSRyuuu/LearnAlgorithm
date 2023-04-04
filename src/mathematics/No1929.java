1767bbfc71290e6feb0465c66d122a2588d3483f
HyunSik Ryu <hsryu123@naver.com>
2023-04-05 00:48:53 +0900

    BOJ

---
+++ src/mathematics/No1929.java
@@ -0,0 +1,44 @@
+package mathematics;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class No1929 {
+    // 1929번 : 소수 구하기
+    static StringBuilder sb = new StringBuilder();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int m = Integer.parseInt(st.nextToken());
+        int n = Integer.parseInt(st.nextToken());
+        answer(m,n);
+        System.out.println(sb);
+    }
+    static void answer(int m,int n){
+        if(m==2){
+            sb.append(2).append("\n");
+            m+=1;
+        }else if(m==1){
+            sb.append(2).append("\n");
+            m=3;
+        } else if(m%2==0){
+            m+=1;
+        }
+
+        while(m<=n){
+            boolean YN = check(m);
+            if(YN){
+                sb.append(m).append("\n");
+            }
+            m+=2;
+        }
+    }
+    static boolean check(int m){
+        for(int i=2;i<=(int)Math.sqrt(m);i++){
+            if(m%i==0) return false;
+        }
+        return true;
+    }
+}
