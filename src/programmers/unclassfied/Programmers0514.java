9ed7af53953fca883e7ed10e35231ecc2e4cf404
HyunSik Ryu <hsryu123@naver.com>
2023-05-14 20:23:58 +0900

    Programmers LV1 추억점수

---
+++ src/programmers/unclassfied/Programmers0514.java
@@ -0,0 +1,38 @@
+package programmers.unclassfied;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * 추억 점수
+ * LV1
+ */
+public class Programmers0514 {
+    public static void main(String[] args) {
+            //입력 값
+            String[] name = {"may", "kein", "kain", "radi"};
+            int[] yearning = {5, 10, 1, 3};
+            String[][] photo = {{"may", "kein", "kain", "radi"},{"may", "kein", "brin", "deny"}, {"kon", "kain", "may", "coni"}};
+            System.out.println(Arrays.toString(solution(name, yearning, photo)));
+        }
+    static int[] solution(String[] name, int[] yearning, String[][] photo) {
+        int[] answer = new int[photo.length];
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0;i<name.length;i++){
+            map.put(name[i],yearning[i]);
+        }
+        for(int i=0;i<photo.length;i++){
+            int sum = 0;
+            for(int j=0;j<photo[i].length;j++){
+                if(map.containsKey(photo[i][j])){
+                    sum+=map.get(photo[i][j]);
+                }
+            }
+            answer[i] = sum;
+        }
+        return answer;
+    }
+}
+
+
