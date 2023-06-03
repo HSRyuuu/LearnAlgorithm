204a6db9e3e1626b709f0766032540ff41955dbd
HyunSik Ryu <hsryu123@naver.com>
2023-06-03 22:11:49 +0900

    2805 나무 자르기/ 이분탐색 / 실버2

---
+++ src/baekjoon/search/binary_search/No2805.java
@@ -0,0 +1,54 @@
+package baekjoon.search.binary_search;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+/**
+ * 2805번 : 나무 자르기
+ * 이분탐색, 매개변수 탐색
+ * 실버2
+ */
+public class No2805 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader((System.in)));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        int[] trees = new int[N];
+        int max = 0;
+        int min = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            trees[i] = Integer.parseInt(st.nextToken());
+            max = Math.max(trees[i],max);
+        }
+
+        //이분탐색
+        while(min<max){
+            int mid = (min+max)/2;
+            long sum = cutting(trees,mid);
+
+            if(sum < M){
+                max = mid;
+            }else{
+                min = mid+1;
+            }
+        }
+
+        System.out.println(min-1);
+    }
+    static long cutting(int[] trees, int height){
+        long result = 0;
+        for(int i=0;i<trees.length;i++){
+            if(trees[i] > height){
+                result += trees[i] - height;
+            }
+        }
+        return  result;
+    }
+}
