21ff3572d738f45b0230c5ea19854e31ead860f2
HyunSik Ryu <hsryu123@naver.com>
2023-07-13 23:50:43 +0900

    DP

---
+++ src/CodingTest/DP_dynamic_programming/silver/No2579.java
@@ -0,0 +1,34 @@
+package CodingTest.DP_dynamic_programming.silver;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class No2579 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int[] DP = new int[N + 1];
+        int[] arr = new int[N + 1];
+
+
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        DP[1] = arr[1];
+
+        if (N >= 2) {
+            DP[2] = arr[1] + arr[2];
+        }
+
+        for (int i = 3; i <= N; i++) {
+            DP[i] = Math.max(DP[i - 2] , DP[i - 3] + arr[i - 1]) + arr[i];
+        }
+
+        System.out.println(DP[N]);
+
+    }
+}
